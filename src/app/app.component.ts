4263f7244ad5e98e9c182edc666a9717e8703036
dinmiranda <dinmiranda@gmail.com>
2016-12-27 08:43:19 +0800

    Multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -28,7 +24,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes = HEROES;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
