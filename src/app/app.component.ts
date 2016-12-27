865d0ac7f080f888e6f2556f0b7fa1550099e9ef
dinmiranda <dinmiranda@gmail.com>
2016-12-27 10:12:38 +0800

    Routers broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-
 }
